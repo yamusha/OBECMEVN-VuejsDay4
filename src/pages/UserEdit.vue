a9a4821d415d96c84f3e5bcaa904c74cb1e80d8d
suttiruk srisawat <mhukdham@gmail.com>
2021-09-09 16:04:42 +0700

    Day4

--- src/pages/UserEdit.vue
+++ src/pages/UserEdit.vue
@@ -2,9 +2,14 @@
   <div>
     <button class="button" @click="$router.go(-1)"> Back </button>
     <h1 class="title">User Edit {{$route.params.id}}</h1>
-
+  <form>
     <input type="text" class="input" v-model="user.username">
     <input type="text" class="input" v-model="user.email">
+    <button @click="saveUser(user.id)" class="button is-link" type="button">Save</button>
+    <router-link to="/user" class="button is-link is-light">Cancel</router-link>  
+    
+  </form>
+    
     
     <!-- <pre>
       {{user}}
@@ -27,5 +32,15 @@ export default {
       user: []
     }
   },
+  methods: {
+    async saveUser(id) {
+      try {
+        const res =  await axios.put(url + '/' + id)
+        console.log(res)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  }
 }
 </script>
